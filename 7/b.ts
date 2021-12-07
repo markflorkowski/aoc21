45901d95cbd9ec9a5ecf0cd05f5625b99d8800a9
Mark R. Florkowski <mark.florkowski@gmail.com>
2021-12-06 21:55:29 -0800

    math is hard :(

--- 7/b.ts
+++ 7/b.ts
@@ -4,34 +4,14 @@ const ipt = Deno.readTextFileSync("./input.txt")
   .split(",")
   .map((x) => parseInt(x));
 
-//console.log(ipt);
-
-const median = (values: number[]) => {
-  values.sort(function (a, b) {
-    return a - b;
-  });
-
-  const half = Math.floor(values.length / 2);
-
-  if (values.length % 2) return values[half];
-
-  return (values[half - 1] + values[half]) / 2.0;
-};
-
 const average = (array: number[]) =>
   array.reduce((a, b) => a + b) / array.length;
 
 const avg = Math.floor(average(ipt));
 
 const cost = (x: number, r: number) => {
-  let fuel = 0;
   let steps = Math.abs(r - x);
-  while (steps > 0) {
-    fuel += steps;
-    steps--;
-  }
-
-  return fuel;
+  return (steps * (steps + 1)) / 2;
 };
 
 let sum = 0;
