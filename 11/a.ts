2e3dad8168b52e9eb0b967a20dd1204f4cc6ee91
Mark R. Florkowski <mark.florkowski@gmail.com>
2021-12-10 22:40:20 -0800

    day 11

---
+++ 11/a.ts
@@ -0,0 +1,63 @@
+export {};
+
+const grid = Deno.readTextFileSync("./input.txt")
+  .split("\n")
+  .map((x) => x.split("").map((y) => parseInt(y)));
+
+//console.log(ipt);
+
+const neighbors = (x: number, y: number) => {
+  return [
+    { x: x - 1, y },
+    { x: x - 1, y: y - 1 },
+    { x: x - 1, y: y + 1 },
+
+    { x, y: y - 1 },
+    { x, y: y + 1 },
+
+    { x: x + 1, y },
+    { x: x + 1, y: y + 1 },
+    { x: x + 1, y: y - 1 },
+  ];
+};
+
+const flash = (pt: { x: number; y: number }) => {
+  if (grid[pt.x]?.[pt.y] === undefined) return;
+
+  grid[pt.x][pt.y] += 1;
+
+  if (grid[pt.x][pt.y] == 10) {
+    flashes++;
+    neighbors(pt.x, pt.y).forEach((n) => flash(n));
+  }
+};
+
+let flashes = 0;
+const step = () => {
+  //increment all
+  grid.forEach((row, x) => {
+    row.forEach((_, y) => {
+      flash({ x, y });
+    });
+  });
+
+  grid.forEach((row, x) => {
+    row.forEach((cur, y) => {
+      if (cur > 9) {
+        grid[x][y] = 0;
+      }
+    });
+  });
+
+  console.log("\n", grid.map((x) => x.join("")).join("\n"));
+
+  return grid;
+};
+
+let i = 0;
+while (i < 100) {
+  step();
+  i++;
+}
+
+console.log(flashes);
