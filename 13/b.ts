1b7425178f9de058aefd735da0716c853660a0fe
Mark R. Florkowski <mark.florkowski@gmail.com>
2021-12-13 23:00:13 -0800

    13 cleanup

--- 13/b.ts
+++ 13/b.ts
@@ -1,27 +1,5 @@
 export {};
 
-let test = `6,10
-0,14
-9,10
-0,3
-10,4
-4,11
-6,0
-6,12
-4,1
-0,13
-10,12
-3,4
-3,0
-8,4
-1,10
-2,14
-8,10
-9,0
-
-fold along y=7
-fold along x=5`;
-
 const ipt = Deno.readTextFileSync("./input.txt").split("\n\n")[0].split("\n");
 
 const inst = Deno.readTextFileSync("./input.txt")
